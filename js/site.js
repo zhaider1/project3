7ae55979db96fdcf5416607f5f80d7964bf79a4c
Zayaan Haider <Zayaan@Zayaans-MacBook-Pro.local>
2017-12-07 22:08:25 -0600

    added music playing functionality to the image

--- js/site.js
+++ js/site.js
@@ -2,10 +2,26 @@ $('html').addClass('js').removeClass('no-js');
 $.noConflict
 
 (function($) {
-  var music = new Audio('images/Spooky.mp3');
+  var song = new Audio('images/Spooky.mp3');
 
   function play() {
-    music.play();
+    song.play();
   }
-  
-})
+  $('#song').remove();
+
+  $('#spook').onclick(play);
+
+  $(document).keypress(function(e) {
+    if(e.keyCode === 32) {
+      if(song.paused === true) {
+        song.play();
+      }
+      else {
+        song.pause();
+      }
+      }
+    }
+  }
+
+
+})(jQuery);
